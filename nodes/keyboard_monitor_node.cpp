c4935d72b7058fc353ed3eaf6a3650ab3e77c4f3
dspangpang <873727286@qq.com>
2023-04-11 22:16:51 +0800

    提供修改了节点的发布频率的功能

--- nodes/keyboard_monitor_node.cpp
+++ nodes/keyboard_monitor_node.cpp
@@ -9,10 +9,15 @@ int main(int argc, char ** argv){
 
     ros::init(argc, argv, "keyboard_monitor_node");
     ros::NodeHandle nh;
+    double frequency = 35;
 
     KeyboardMonitor km(nh);
 
-    ros::Rate rate(35);
+    const auto& tmp_node_name = ros::this_node::getName();
+
+    nh.getParam(tmp_node_name + "/pub_frequency",frequency);
+
+    ros::Rate rate(frequency);
 
     km.start();
 
